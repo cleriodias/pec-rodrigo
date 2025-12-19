470487160f13da96ce4551d7809a094b6b55b775
clerídias <cleriodias@gmail.com>
2025-12-19 14:34:35 +0000

    nao-sei-oque-foi-feito

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -106,7 +106,7 @@ export default function Login({ status, canResetPassword, units = [] }) {
                         Escolha a unidade
                     </p>
                     {units.length ? (
-                        <div className="mt-3 grid gap-3 sm:grid-cols-2">
+                        <div className="mt-3 grid gap-3 sm:grid-cols-4">
                             {units.map((unit) => (
                                 <button
                                     type="button"
