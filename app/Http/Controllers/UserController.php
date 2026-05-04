2b32ebcd05c09161a2a2f3b5bda8da0a2fc6475e
clerídias <cleriodias@gmail.com>
2026-05-04 04:00:53 +0100

    Update two function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -338,16 +338,13 @@ class UserController extends Controller
             'email' => $request->email,
             'phone' => $request->phone ?: null,
             'funcao' => $request->funcao,
+            'funcao_original' => $request->funcao,
             'hr_ini' => $request->hr_ini,
             'hr_fim' => $request->hr_fim,
             'salario' => $request->salario,
             'vr_cred' => $request->vr_cred,
             'tb2_id' => $primaryUnit,
         ];
-
-        if ($user->funcao_original === null) {
-            $updateData['funcao_original'] = $user->funcao ?? $request->funcao;
-        }
 
         $user->update($updateData);
 
